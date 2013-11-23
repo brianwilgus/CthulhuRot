d75f432e5316c046ea4113db27e7ab18226d646f
Brian Wilgus <contact@brianwilgus.com>
2013-11-23 11:21:37 -0500

    items and repositories

--- js/entities.js
+++ js/entities.js
@@ -5,54 +5,135 @@ Game.Mixins = {};
 //=======================
 // Define our Moveable mixin
 Game.Mixins.Moveable = {
-	    name: 'Moveable',
-	    tryMove: function(x, y, z, map) {
-	        var map = this.getMap();
-	        // Must use starting z
-	        var tile = map.getTile(x, y, this.getZ());
-	        var target = map.getEntityAt(x, y, this.getZ());
-	        // If our z level changed, check if we are on stair
-	        if (z < this.getZ()) {
-	            if (tile != Game.Tile.stairsUpTile) {
-	                Game.sendMessage(this, "You can't go up here!");
-	            } else {
-	                Game.sendMessage(this, "You ascend to level %d!", [z + 1]);
-	                this.setPosition(x, y, z);
-	            }
-	        } else if (z > this.getZ()) {
-	            if (tile != Game.Tile.stairsDownTile) {
-	                Game.sendMessage(this, "You can't go down here!");
-	            } else {
-	                this.setPosition(x, y, z);
-	                Game.sendMessage(this, "You descend to level %d!", [z + 1]);
-	            }
-	        // If an entity was present at the tile
-	        } else if (target) {
-	            // If we are an attacker, try to attack
-	            // the target
-	            if (this.hasMixin('Attacker')) {
-	                this.attack(target);
-	                return true;
-	            } else {
-	                // If not nothing we can do, but we can't 
-	                // move to the tile
-	                return false;
-	            }
-	        // Check if we can walk on the tile
-	        // and if so simply walk onto it
-	        } else if (tile.isWalkable()) {        
-	            // Update the entity's position
-	            this.setPosition(x, y, z);
-	            return true;
-	        // Check if the tile is diggable, and
-	        // if so try to dig it
-	        } else if (tile.isDiggable()) {
-	            map.dig(x, y, z);
-	            return true;
-	        }
-	        return false;
-	    }
+    name: 'Moveable',
+    tryMove: function(x, y, z, map) {
+        var map = this.getMap();
+        // Must use starting z
+        var tile = map.getTile(x, y, this.getZ());
+        var target = map.getEntityAt(x, y, this.getZ());
+        // If our z level changed, check if we are on stair
+        if (z < this.getZ()) {
+            if (tile != Game.Tile.stairsUpTile) {
+                Game.sendMessage(this, "You can't go up here!");
+            } else {
+                Game.sendMessage(this, "You ascend to level %d!", [z + 1]);
+                this.setPosition(x, y, z);
+            }
+        } else if (z > this.getZ()) {
+            if (tile != Game.Tile.stairsDownTile) {
+                Game.sendMessage(this, "You can't go down here!");
+            } else {
+                this.setPosition(x, y, z);
+                Game.sendMessage(this, "You descend to level %d!", [z + 1]);
+            }
+        // If an entity was present at the tile
+        } else if (target) {
+            // If we are an attacker, try to attack
+            // the target
+            if (this.hasMixin('Attacker')) {
+                this.attack(target);
+                return true;
+            } else {
+                // If not nothing we can do, but we can't 
+                // move to the tile
+                return false;
+            }
+        // Check if we can walk on the tile
+        // and if so simply walk onto it
+        } else if (tile.isWalkable()) {        
+            // Update the entity's position
+            this.setPosition(x, y, z);
+            return true;
+        // Check if the tile is diggable, and
+        // if so try to dig it
+        } else if (tile.isDiggable()) {
+            map.dig(x, y, z);
+            return true;
+        }
+        return false;
+    }
+};
+
+//ACTORS
+//=======================
+//Main player's actor mixin
+Game.Mixins.PlayerActor = {
+	name: 'PlayerActor',
+	groupName: 'Actor',
+	act: function() {
+      // Detect if the game is over
+      if (this.getHp() < 1) {
+          Game.Screen.playScreen.setGameEnded(true);
+          // Send a last message to the player
+          Game.sendMessage(this, 'You have died... Press [Enter] to continue!');
+      }
+	   // Re-render the screen
+	   Game.refresh();
+	   
+	   // Lock the engine and wait asynchronously
+	   // for the player to press a key.
+	   this.getMap().getEngine().lock();           
+     
+	   // Clear the message queue
+     this.clearMessages();     
 	}
+}
+
+Game.Mixins.FungusActor = {
+	name: 'FungusActor',
+	groupName: 'Actor',
+	init: function() {
+		this._growthsRemaining = 5;
+	},
+	act: function() { 
+      // Check if we are going to try growing this turn
+      if (this._growthsRemaining > 0) {
+          if (Math.random() <= 0.02) {
+              // Generate the coordinates of a random adjacent square by
+              // generating an offset between [-1, 0, 1] for both the x and
+              // y directions. To do this, we generate a number from 0-2 and then
+              // subtract 1.
+              var xOffset = Math.floor(Math.random() * 3) - 1;
+              var yOffset = Math.floor(Math.random() * 3) - 1;
+              // Make sure we aren't trying to spawn on the same tile as us
+              if (xOffset != 0 || yOffset != 0) {
+                  // Check if we can actually spawn at that location, and if so
+                  // then we grow!
+              	if (this.getMap().isEmptyFloor(this.getX() + xOffset,
+					                               this.getY() + yOffset,
+					                               this.getZ())) {
+						 var entity = new Game.Entity(Game.FungusTemplate);
+						 entity.setPosition(this.getX() + xOffset, this.getY() + yOffset, this.getZ());
+						 this.getMap().addEntity(entity);
+						 this._growthsRemaining--;
+						 // Send a message nearby!
+						 Game.sendMessageNearby(this.getMap(),
+						     entity.getX(), entity.getY(), entity.getZ(),
+						     'The fungus is spreading!');
+					}
+              }
+          }
+      }
+  }
+};
+
+Game.Mixins.WanderActor = {
+  name: 'WanderActor',
+  groupName: 'Actor',
+  act: function() {
+      // Flip coin to determine if moving by 1 in the positive or negative direction
+      var moveOffset = (Math.round(Math.random()) === 1) ? 1 : -1;
+      // Flip coin to determine if moving in x direction or y direction
+      if (Math.round(Math.random()) === 1) {
+          this.tryMove(this.getX() + moveOffset, this.getY(), this.getZ());
+      } else {
+          this.tryMove(this.getX(), this.getY() + moveOffset, this.getZ());
+      }
+  }
+};
+
+//=======================
+// END ACTORS
 
 Game.Mixins.Destructible = {
     name: 'Destructible',
@@ -86,7 +167,7 @@ Game.Mixins.Destructible = {
             }
         }
     }
-}
+};
 
 Game.Mixins.Attacker = {
     name: 'Attacker',
@@ -115,7 +196,7 @@ Game.Mixins.Attacker = {
             target.takeDamage(this, damage);
         }
     }
-}
+};
 
 Game.Mixins.MessageRecipient = {
     name: 'MessageRecipient',
@@ -131,7 +212,7 @@ Game.Mixins.MessageRecipient = {
     clearMessages: function() {
         this._messages = [];
     }
-}
+};
 
 //This signifies our entity posseses a field of vision of a given radius.
 Game.Mixins.Sight = {
@@ -143,84 +224,6 @@ Game.Mixins.Sight = {
   getSightRadius: function() {
       return this._sightRadius;
   }
-}
-
-//ACTORS
-//=======================
-//Main player's actor mixin
-Game.Mixins.PlayerActor = {
-	name: 'PlayerActor',
-	groupName: 'Actor',
-	act: function() {
-        // Detect if the game is over
-        if (this.getHp() < 1) {
-            Game.Screen.playScreen.setGameEnded(true);
-            // Send a last message to the player
-            Game.sendMessage(this, 'You have died... Press [Enter] to continue!');
-        }
-	   // Re-render the screen
-	   Game.refresh();
-	   
-	   // Lock the engine and wait asynchronously
-	   // for the player to press a key.
-	   this.getMap().getEngine().lock();           
-       
-	   // Clear the message queue
-       this.clearMessages();     
-	}
-}
-
-Game.Mixins.FungusActor = {
-	name: 'FungusActor',
-	groupName: 'Actor',
-	init: function() {
-		this._growthsRemaining = 5;
-	},
-	act: function() { 
-        // Check if we are going to try growing this turn
-        if (this._growthsRemaining > 0) {
-            if (Math.random() <= 0.02) {
-                // Generate the coordinates of a random adjacent square by
-                // generating an offset between [-1, 0, 1] for both the x and
-                // y directions. To do this, we generate a number from 0-2 and then
-                // subtract 1.
-                var xOffset = Math.floor(Math.random() * 3) - 1;
-                var yOffset = Math.floor(Math.random() * 3) - 1;
-                // Make sure we aren't trying to spawn on the same tile as us
-                if (xOffset != 0 || yOffset != 0) {
-                    // Check if we can actually spawn at that location, and if so
-                    // then we grow!
-                	if (this.getMap().isEmptyFloor(this.getX() + xOffset,
-					                               this.getY() + yOffset,
-					                               this.getZ())) {
-						 var entity = new Game.Entity(Game.FungusTemplate);
-						 entity.setPosition(this.getX() + xOffset, this.getY() + yOffset, this.getZ());
-						 this.getMap().addEntity(entity);
-						 this._growthsRemaining--;
-						 // Send a message nearby!
-						 Game.sendMessageNearby(this.getMap(),
-						     entity.getX(), entity.getY(), entity.getZ(),
-						     'The fungus is spreading!');
-					}
-                }
-            }
-        }
-    }
-}
-
-Game.Mixins.WanderActor = {
-    name: 'WanderActor',
-    groupName: 'Actor',
-    act: function() {
-        // Flip coin to determine if moving by 1 in the positive or negative direction
-        var moveOffset = (Math.round(Math.random()) === 1) ? 1 : -1;
-        // Flip coin to determine if moving in x direction or y direction
-        if (Math.round(Math.random()) === 1) {
-            this.tryMove(this.getX() + moveOffset, this.getY(), this.getZ());
-        } else {
-            this.tryMove(this.getX(), this.getY() + moveOffset, this.getZ());
-        }
-    }
 };
 
 
@@ -236,18 +239,21 @@ Game.PlayerTemplate = {
 	mixins: [Game.Mixins.PlayerActor,
 	         Game.Mixins.Attacker, Game.Mixins.Destructible,
 	         Game.Mixins.Sight, Game.Mixins.MessageRecipient]
-}
+};
 
-//Fungus template
-Game.FungusTemplate = {
-	name: 'fungus',
-	character: 'F',
-	foreground: 'green',
+
+//Create our central entity repository
+Game.EntityRepository = new Game.Repository('entities', Game.Entity);
+
+Game.EntityRepository.define('fungus', {
+    name: 'fungus',
+    character: 'F',
+    foreground: 'green',
     maxHp: 10,
-	mixins: [Game.Mixins.FungusActor, Game.Mixins.Destructible]
-}
+    mixins: [Game.Mixins.FungusActor, Game.Mixins.Destructible]
+});
 
-Game.BatTemplate = {
+Game.EntityRepository.define('bat', {
     name: 'bat',
     character: 'B',
     foreground: 'white',
@@ -255,9 +261,9 @@ Game.BatTemplate = {
     attackValue: 4,
     mixins: [Game.Mixins.WanderActor, 
              Game.Mixins.Attacker, Game.Mixins.Destructible]
-};
+});
 
-Game.NewtTemplate = {
+Game.EntityRepository.define('newt', {
     name: 'newt',
     character: ':',
     foreground: 'yellow',
@@ -265,7 +271,7 @@ Game.NewtTemplate = {
     attackValue: 2,
     mixins: [Game.Mixins.WanderActor, 
              Game.Mixins.Attacker, Game.Mixins.Destructible]
-};
+});
 
 
 
